7c1e3532f294c84910a6983c8f61110294423ac8
NicolaijScheu <nicolaij.scheu@gmx.de>
2022-11-30 11:59:15 +0100

    contact me form working

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,9 +53,6 @@ skillsHeader.forEach((header) => {
 const tabs = document.querySelectorAll("[data-target]");
 const tabContents = document.querySelectorAll("[data-content]");
 
-console.log("tabs: ", tabs);
-console.log("tabContents: ", tabContents);
-
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.target);
@@ -136,3 +133,64 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/* Send Mail */
+var form_id_js = "contact-form"
+
+var data_js = {
+  "access_token": "b701047mtupiimtjt3xtog3f"
+};
+
+/* function js_onSuccess() {
+  // remove this to avoid redirect
+  window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+}
+
+function js_onError(error) {
+  // remove this to avoid redirect
+  window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
+} */
+
+var sendButton = document.getElementById("js-send");
+
+function js_send() {
+  sendButton.disabled=true;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+          console.log("success");
+      } else
+      if(request.readyState == 4) {
+        console.log("success");
+      }
+  };
+
+  var subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
+  var message = document.querySelector("#" + form_id_js + " [name='text']").value;
+  data_js['subject'] = subject;
+  data_js['text'] = message;
+  var params = toParams(data_js);  
+
+  request.open("POST", "https://postmail.invotes.com/send", true);
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  request.send(params);
+
+  return false;
+}
+
+sendButton.onclick = js_send;
+
+function toParams(data_js) {
+    var form_data = [];
+    for ( var key in data_js ) {
+        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
+    }
+
+    return form_data.join("&");
+}
+
+var js_form = document.getElementById(form_id_js);
+js_form.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
