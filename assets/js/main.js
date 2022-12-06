ac283ce0c2559e859ec22ae497f4b90d45fc7bdf
NicolaijScheu <nicolaij.scheu@gmx.de>
2022-12-06 18:04:20 +0100

    clean up contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,76 +69,88 @@ tabs.forEach((tab) => {
   });
 });
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
-    const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp)
+window.addEventListener("scroll", scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /* Send Mail */
-var form_id_js = "contact-form"
+var form_id_js = "contact-form";
 
 var data_js = {
-  "access_token": "b701047mtupiimtjt3xtog3f"
+  access_token: "b701047mtupiimtjt3xtog3f",
 };
 
 /* function js_onSuccess() {
@@ -154,27 +166,47 @@ function js_onError(error) {
 var sendButton = document.getElementById("js-send");
 
 function js_send() {
-  sendButton.disabled=true;
+  sendButton.disabled = true;
   var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-      if (request.readyState == 4 && request.status == 200) {
-          console.log("success");
-      } else
-      if(request.readyState == 4) {
-        console.log("success");
-      }
+  request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+      console.log("success");
+    } else if (request.readyState == 4) {
+      console.log("failure");
+    }
   };
 
-  var subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
-  var message = document.querySelector("#" + form_id_js + " [name='text']").value;
-  data_js['subject'] = subject;
-  data_js['text'] = message;
-  var params = toParams(data_js);  
+  var subject = document.querySelector(
+    "#" + form_id_js + " [name='subject']"
+  ).value;
+  var message = document.querySelector(
+    "#" + form_id_js + " [name='text']"
+  ).value;
+
+  var nameField = document.getElementById("name-form");
+  var emailField = document.getElementById("email-form");
+  var messageField = document.getElementById("message-form");
+
+  data_js["subject"] = subject;
+  data_js["text"] = message;
+  var params = toParams(data_js);
 
   request.open("POST", "https://postmail.invotes.com/send", true);
   request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
   request.send(params);
+
+  sendButton.classList.add("button__disabled");
+  console.log("im waiting");
+
+  function resetSendButton() {
+    sendButton.classList.remove("button__disabled");
+  }
+  setTimeout(resetSendButton, 3000);
+
+  nameField.value = "";
+  emailField.value = "";
+  messageField.value = "";
 
   return false;
 }
@@ -182,15 +214,17 @@ function js_send() {
 sendButton.onclick = js_send;
 
 function toParams(data_js) {
-    var form_data = [];
-    for ( var key in data_js ) {
-        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
-    }
+  var form_data = [];
+  for (var key in data_js) {
+    form_data.push(
+      encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key])
+    );
+  }
 
-    return form_data.join("&");
+  return form_data.join("&");
 }
 
 var js_form = document.getElementById(form_id_js);
 js_form.addEventListener("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 });
